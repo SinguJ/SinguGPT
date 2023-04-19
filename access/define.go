57bcab21f9ae68976c7e0e2633ded94834ba2241
Singu <singu@singu.top>
2023-04-20 00:04:34 +0800

    定义用户变更事件及用户变更事件的处理机制

--- access/define.go
+++ access/define.go
@@ -15,10 +15,21 @@ import (
 //     err       => 错误
 type MessageHandler func(sessionId, requestId string, user *models.User, contents models.Contents) (resp models.Contents, err error)
 
+// BatchUserChangeHandler 批量用户变更处理器
+//   Parameters:
+//     users => 会话 ID
+//       <KEY>  => 用户变更事件对象
+//       <VAL>  => 被变更的用户对象
+//   Returns:
+//     err   => 错误
+type BatchUserChangeHandler func(users map[UserChangeEvent]*models.User) error
+
 // Dispatcher 调度器
 type Dispatcher interface {
     // OnMessageReceive 接收消息事件
     OnMessageReceive(MessageHandler) error
+    // OnBatchUserChange 批量用户变更事件
+    OnBatchUserChange(BatchUserChangeHandler) error
     // Listen 启动监听
     // 该监听函数不可以阻塞程序继续运行
     Listen() error
