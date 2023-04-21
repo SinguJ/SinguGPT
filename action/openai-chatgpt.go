e062af5d1c2bf9ec8b5bdb2721d05422349001a4
Singu <singu@singu.top>
2023-04-22 03:28:54 +0800

    改造 action 模式，适配 Contents 结构

--- action/openai-chatgpt.go
+++ action/openai-chatgpt.go
@@ -3,11 +3,22 @@ package action
 import (
     "SinguGPT/ai/openai"
     "SinguGPT/models"
+    "SinguGPT/utils"
+    "strings"
 )
 
 func init() {
-    RegisterActionFunc(func(sessionId string, requestId string, user *models.User, content string) (resp string, err error) {
+    RegisterActionFunc(func(sessionId string, _ string, user *models.User, _ models.Contents, contents models.Contents) (models.Contents, error) {
+        content := strings.Join(utils.Map(contents.Find(models.TagBody), func(content models.Content) string {
+            return content.ToString()
+        }), "\n")
         req := openai.NewChatRequest(sessionId, user, content)
-        return openai.Chat(req)
+        resp, err := openai.Chat(req)
+        if err != nil {
+            return nil, err
+        }
+        return models.Contents{
+            models.NewTextContent(models.TagBody, resp),
+        }, nil
     }, "ChatGPT", "GPT3", "GPT-3", "GPT3.0", "GPT-3.0")
 }
