2e2c8df931e8990f90c7e7c18ecb4f13526e6056
Singu <singu@singu.top>
2023-04-22 04:34:28 +0800

    优化 `ChatGPT` 回复内容中，中英文掺杂内容的显示效果

--- action/openai-chatgpt.go
+++ action/openai-chatgpt.go
@@ -4,8 +4,22 @@ import (
     "SinguGPT/ai/openai"
     "SinguGPT/models"
     "SinguGPT/utils"
+    "regexp"
     "strings"
 )
+
+var __front__ = regexp.MustCompile(`(\p{Han}+)([a-zA-Z]+)`)
+var __behind__ = regexp.MustCompile(`([a-zA-Z]+)(\p{Han}+)`)
+
+// 在汉字与英语单词之间添加空格
+func addSpacesBetweenChineseCharactersAndEnglishWords(str string) string {
+    return __front__.ReplaceAllString(__behind__.ReplaceAllString(str, "$1 $2"), "$1 $2")
+}
+
+// 内容优化
+func responseOptimization(resp string) string {
+    return addSpacesBetweenChineseCharactersAndEnglishWords(resp)
+}
 
 func init() {
     RegisterActionFunc(func(sessionId string, _ string, user *models.User, _ models.Contents, contents models.Contents) (models.Contents, error) {
@@ -17,6 +31,8 @@ func init() {
         if err != nil {
             return nil, err
         }
+        // 优化响应内容
+        resp = responseOptimization(resp)
         markdown := models.NewMarkdownContent(models.TagBody, resp)
         return models.Contents{
             markdown,
