8000a0184ed2b0cc7b26ea152c0d483a0b876d1f
Singu <singu@singu.top>
2023-04-24 14:44:11 +0800

    修复读取单分段内容的邮件会崩溃问题

--- access/email/imap/funcs.go
+++ access/email/imap/funcs.go
@@ -32,11 +32,67 @@ func getBody(msg *imapclient.FetchMessageBuffer) []byte {
     return nil
 }
 
-func readBody(msg *imapclient.FetchMessageBuffer) ([]*Content, []*Attach) {
-    if msg.BodySection == nil {
-        return []*Content{}, []*Attach{}
+func readPart (part *mailLib.Part) (*Content, *Attach) {
+    switch part.Header.(type) {
+    case *mailLib.InlineHeader:
+        inlineHeader := part.Header.(*mailLib.InlineHeader)
+        // 构建内容对象
+        content := Content{}
+        // 读取内容类型
+        contentType, _, err := inlineHeader.ContentType()
+        if err != nil {
+            panic(errors.Wrap(err))
+        }
+        switch contentType {
+        case "text/plain":
+            content.Type = Text
+        case "text/html":
+            content.Type = HTML
+        default:
+            content.Type = Other
+        }
+        // 读取内容长度
+        content.Len = inlineHeader.Len()
+        // 读取内容文本
+        data, err := io.ReadAll(part.Body)
+        if err != nil {
+            panic(errors.Wrap(err))
+        }
+        content.Text = string(data)
+        return &content, nil
+    case *mailLib.AttachmentHeader:
+        attachmentHeader := part.Header.(*mailLib.AttachmentHeader)
+        // 构建附件对象
+        attach := Attach{}
+        // 读取内容类型
+        contentType, _, err := attachmentHeader.ContentType()
+        if err != nil {
+            panic(errors.Wrap(err))
+        }
+        switch contentType {
+        // TODO: 补充文件的 ContentType
+        default:
+            attach.Type = Other
+        }
+        // 读取文件名
+        filename, err := attachmentHeader.Filename()
+        if err != nil {
+            panic(errors.Wrap(err))
+        }
+        attach.Filename = filename
+        // 读取文件数据&文件大小
+        data, err := io.ReadAll(part.Body)
+        if err != nil {
+            panic(errors.Wrap(err))
+        }
+        attach.Bytes = data
+        attach.Size = len(data)
+        return nil, &attach
     }
-    bodyStructure := msg.BodyStructure.(*imap.BodyStructureMultiPart)
+    return nil, nil
+}
+
+func readMultiPartBody (msg *imapclient.FetchMessageBuffer, bodyStructure *imap.BodyStructureMultiPart) (*mailLib.Reader, error) {
     boundary := bodyStructure.Extended.Params["boundary"]
     //goland:noinspection SpellCheckingInspection
     tag := "IMTHEBOUNDARY"
@@ -44,7 +100,27 @@ func readBody(msg *imapclient.FetchMessageBuffer) ([]*Content, []*Attach) {
     prefix := fmt.Sprintf("Mime-Version: 1.0\r\nContent-Type: %s; boundary=%s\r\n\r\n", mediaType, tag)
     text := string(getBody(msg))
     text = prefix + strings.ReplaceAll(text, boundary, tag)
-    reader, err := mailLib.CreateReader(bytes.NewReader([]byte(text)))
+    return mailLib.CreateReader(bytes.NewReader([]byte(text)))
+}
+
+func readSinglePartBody (msg *imapclient.FetchMessageBuffer, bodyStructure *imap.BodyStructureSinglePart) (*mailLib.Reader, error) {
+    prefix := fmt.Sprintf("Content-Type: %s\nContent-Transfer-Encoding: %s\n\n", bodyStructure.MediaType(), bodyStructure.Encoding)
+    text := prefix + string(getBody(msg))
+    return mailLib.CreateReader(bytes.NewReader([]byte(text)))
+}
+
+func readBody(msg *imapclient.FetchMessageBuffer) ([]*Content, []*Attach) {
+    if msg.BodySection == nil {
+        return []*Content{}, []*Attach{}
+    }
+    var reader *mailLib.Reader
+    var err error
+    switch bodyStructure := msg.BodyStructure.(type) {
+    case *imap.BodyStructureMultiPart:
+        reader, err = readMultiPartBody(msg, bodyStructure)
+    case *imap.BodyStructureSinglePart:
+        reader, err = readSinglePartBody(msg, bodyStructure)
+    }
     if err != nil {
         panic(errors.Wrap(err))
     }
@@ -59,61 +135,12 @@ func readBody(msg *imapclient.FetchMessageBuffer) ([]*Content, []*Attach) {
             }
             panic(errors.Wrap(err))
         }
-        switch part.Header.(type) {
-        case *mailLib.InlineHeader:
-            inlineHeader := part.Header.(*mailLib.InlineHeader)
-            // 构建内容对象
-            content := Content{}
-            // 读取内容类型
-            contentType, _, err := inlineHeader.ContentType()
-            if err != nil {
-                panic(errors.Wrap(err))
-            }
-            switch contentType {
-            case "text/plain":
-                content.Type = Text
-            case "text/html":
-                content.Type = HTML
-            default:
-                content.Type = Other
-            }
-            // 读取内容长度
-            content.Len = inlineHeader.Len()
-            // 读取内容文本
-            data, err := io.ReadAll(part.Body)
-            if err != nil {
-                panic(errors.Wrap(err))
-            }
-            content.Text = string(data)
-            contents = append(contents, &content)
-        case *mailLib.AttachmentHeader:
-            attachmentHeader := part.Header.(*mailLib.AttachmentHeader)
-            // 构建附件对象
-            attach := Attach{}
-            // 读取内容类型
-            contentType, _, err := attachmentHeader.ContentType()
-            if err != nil {
-                panic(errors.Wrap(err))
-            }
-            switch contentType {
-            // TODO: 补充文件的 ContentType
-            default:
-                attach.Type = Other
-            }
-            // 读取文件名
-            filename, err := attachmentHeader.Filename()
-            if err != nil {
-                panic(errors.Wrap(err))
-            }
-            attach.Filename = filename
-            // 读取文件数据&文件大小
-            data, err := io.ReadAll(part.Body)
-            if err != nil {
-                panic(errors.Wrap(err))
-            }
-            attach.Bytes = data
-            attach.Size = len(data)
-            attaches = append(attaches, &attach)
+        content, attach := readPart(part)
+        if content != nil {
+            contents = append(contents, content)
+        }
+        if attach != nil {
+            attaches = append(attaches, attach)
         }
     }
     return contents, attaches
